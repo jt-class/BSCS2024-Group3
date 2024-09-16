86ef8ee9f13aca4e158036275c947314e7f4dc61
Secrettttttt <francisco.paulo.pf@gmail.com>
2024-09-16 18:38:43 +0800

    Created Temporary HomePage

---
+++ Go-Trade/Pages/HomePage.cs
@@ -0,0 +1,15 @@
+namespace Go_Trade.Pages;
+
+public class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}
